5f823d8a5b5bb2132ab2f6d5015f884028832081
TaoSun2016 <kevinsun9516@gmail.com>
2017-05-04 08:24:16 +1200

    add new functions of tool training

---
+++ 04_SourceCode/hrms/CreateToolTraining.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="CreateToolTraining.aspx.cs" Inherits="Default2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div style="height: 100%; width: 100%">
+        <div style="text-align: center">
+            <h2><strong>Attendance Information</strong></h2>
+        </div>
+        <br />
+        <div style="width: 100%">
+            <div style="float: left; width: 25%; text-align: right">
+                Forename:&nbsp;&nbsp;
+            </div>
+            <div style="float: left; width: 25%">
+                <asp:TextBox ID="Forename" runat="server"></asp:TextBox>
+            </div>
+            <div style="float: left; width: 15%; text-align: right">Surname:&nbsp;&nbsp;</div>
+            <div style="float: left; width: 35%">
+                <asp:TextBox ID="Surname" runat="server"></asp:TextBox>
+            </div>
+        </div>
+        <br />
+        <div style="width: 100%; margin-top: 20px">
+            <div style="float: left; width: 25%; text-align: right">
+                Email:&nbsp;&nbsp;
+            </div>
+            <div style="float: left; width: 25%">
+                <asp:TextBox ID="Email" runat="server"></asp:TextBox>
+            </div>
+            <div style="float: left; width: 15%; text-align: right">Date:&nbsp;&nbsp;</div>
+            <div style="float: left; width: 35%">
+                <asp:TextBox ID="Date" runat="server"></asp:TextBox></div>
+        </div>
+        <br />
+                <div style="width: 100%; margin-top: 20px">
+            <div style="float: left; width: 25%; text-align: right">
+                Training Type:&nbsp;&nbsp;
+            </div>
+            <div style="float: left; width: 75%">
+                <asp:DropDownList ID="TrainingType" runat="server">
+                    <asp:ListItem Value="1">I OWN AND AM TRAINED IN THIS TOOL</asp:ListItem>
+                    <asp:ListItem Value="2">I HAVE READ THIS INFORMATION BOOK AND UNDERSTAND THE USE AND CARE</asp:ListItem>
+                    <asp:ListItem Value="3">FOREMAN TRAINED AND WITNESSED GOOD SAFE WORKING PRACTICES</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+        </div>
+        <br />
+        <br />
+        <div style="float: left; width: 100%; text-align: center; margin-top:30px">
+            <asp:Button ID="Add" runat="server" Text="Add" OnClick="Add_Click" />
+        </div>
+    </div>
+</asp:Content>
+
---
+++ 04_SourceCode/hrms/CreateToolTraining.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Add_Click(object sender, EventArgs e)
+    {
+        int ErrCode;
+        string ErrMsg;
+
+
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ToString();
+        SqlConnection conn = new SqlConnection(connectionString);
+
+        SqlCommand myCommand = new SqlCommand("sp_addToolTraining", conn);
+        myCommand.CommandType = CommandType.StoredProcedure;
+
+        SqlParameter cmdParameter = new SqlParameter("@Forname", SqlDbType.VarChar, 50);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = Forename.Text.Trim();
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@Surname", SqlDbType.VarChar,50);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = Surname.Text.Trim();
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@Email", SqlDbType.VarChar,100);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = Email.Text.Trim();
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@Date", SqlDbType.VarChar,8);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = Date.Text;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@TrainingType", SqlDbType.VarChar,30);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = TrainingType.SelectedValue;
+        myCommand.Parameters.Add(cmdParameter);
+
+        
+
+        //////////////////////////////////////////////////////////
+        cmdParameter = new SqlParameter("@ErrCode", SqlDbType.Int);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@ErrMsg", SqlDbType.VarChar, 60);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        conn.Open();
+
+        myCommand.ExecuteNonQuery();
+
+
+        ErrCode = (int)myCommand.Parameters["@ErrCode"].Value;
+        ErrMsg = (string)myCommand.Parameters["@ErrMsg"].Value;
+
+        conn.Close();
+
+        if (ErrCode != 0)
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('Add Error!')</script>");
+
+        }
+        else
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "success", "<script>alert('Add successfully!')</script>");
+        }
+    }
+}
