8b5377ae918165951bdb5411fbe6db216e9138f3
TaoSun2016 <kevinsun9516@gmail.com>
2017-05-04 09:02:50 +1200

    add new functin - change password

---
+++ 04_SourceCode/hrms/ChangePassword.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="ChangePassword.aspx.cs" Inherits="Default2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div style="height: 100%; width: 100%">
+        <div style="text-align: center">
+            <h2><strong>Change Password</strong></h2>
+        </div>
+        <br />
+        <div style="width: 100%">
+            <div style="float: left; width: 25%; text-align: right">
+                User name:&nbsp;&nbsp;
+            </div>
+            <div style="float: left; width: 25%">
+                <asp:TextBox ID="Email" runat="server"></asp:TextBox>*(Email)
+            </div>
+            <div style="float: left; width: 15%; text-align: right">Old Password:&nbsp;&nbsp;</div>
+            <div style="float: left; width: 35%">
+
+                <asp:TextBox ID="OldPassword" runat="server" TextMode="Password"></asp:TextBox>
+
+            </div>
+        </div>
+        <br />
+        <div style="width: 100%; margin-top: 20px">
+            <div style="float: left; width: 25%; text-align: right">
+                New Password:&nbsp;&nbsp;
+            </div>
+            <div style="float: left; width: 25%">
+
+                <asp:TextBox ID="NewPassword" runat="server" TextMode="Password"></asp:TextBox>
+
+            </div>
+            <div style="float: left; width: 15%; text-align: right">Confirm Password:&nbsp;&nbsp;</div>
+            <div style="float: left; width: 35%">
+
+                <asp:TextBox ID="ConfirmPassword" runat="server" TextMode="Password"></asp:TextBox>
+
+            </div>
+
+        <br />
+        <br />
+        <div style="float: left; width: 100%; text-align: center; margin-top: 30px">
+            <asp:Button ID="Change" runat="server" Text="Change" OnClick="Change_Click" />
+        </div>
+    </div>
+</asp:Content>
+
---
+++ 04_SourceCode/hrms/ChangePassword.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Change_Click(object sender, EventArgs e)
+    {
+        int ErrCode;
+        string ErrMsg;
+
+        if (Email.Text.Trim()=="" )
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('Please input User Name!')</script>");
+        }
+        if (OldPassword.Text.Trim() == "" )
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('Please input Old Password!')</script>");
+        }
+        if (NewPassword.Text.Trim() == "")
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('Please input New Password!')</script>");
+        }
+        if (ConfirmPassword.Text.Trim() == "")
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('Please input Confirm Password!')</script>");
+        }
+        if (OldPassword.Text.Trim() == NewPassword.Text.Trim())
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('Old password and new password can not be same!')</script>");
+        }
+        if (ConfirmPassword.Text.Trim() == NewPassword.Text.Trim())
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('Confirm password is not same as new password!')</script>");
+        }
+
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ToString();
+        SqlConnection conn = new SqlConnection(connectionString);
+
+        SqlCommand myCommand = new SqlCommand("sp_changePassword", conn);
+        myCommand.CommandType = CommandType.StoredProcedure;
+
+        SqlParameter cmdParameter = new SqlParameter("@Email", SqlDbType.VarChar, 100);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = Email.Text.Trim();
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@OldPassword", SqlDbType.VarChar,50);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = OldPassword.Text.Trim();
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@NewPassword", SqlDbType.VarChar,100);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = NewPassword.Text.Trim();
+        myCommand.Parameters.Add(cmdParameter);
+
+        
+
+        //////////////////////////////////////////////////////////
+        cmdParameter = new SqlParameter("@ErrCode", SqlDbType.Int);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@ErrMsg", SqlDbType.VarChar, 60);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        conn.Open();
+
+        myCommand.ExecuteNonQuery();
+
+
+        ErrCode = (int)myCommand.Parameters["@ErrCode"].Value;
+        ErrMsg = (string)myCommand.Parameters["@ErrMsg"].Value;
+
+        conn.Close();
+
+        if (ErrCode != 0)
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('"+ErrMsg+"')</script>");
+
+        }
+        else
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "success", "<script>alert('Change password successfully!')</script>");
+        }
+            
+    }
+
+
+
+}
