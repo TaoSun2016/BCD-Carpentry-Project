3cb87487e431202bba40929a945c3d35b8c1859f
TaoSun2016 <kevinsun9516@gmail.com>
2017-05-07 21:22:13 +1200

    Combine the crate, update, delete function of competency informatin in one page

---
+++ 04_SourceCode/hrms/Competency.aspx.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Default2 : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Clear_Fields()
+    {
+        EmployeeID.Value = "";
+        Position.Text = "";
+        OtherPositions.Text = "";
+        Qualifications.Text = "";
+        HireDate.Text = "";
+        YearsInIndustry.Text = "";
+        DriverLicenseClass.Text = "";
+        SiteSafe.ImageUrl = "";
+    }
+
+    protected void Query_Click(object sender, EventArgs e)
+    {
+        int ErrCode;
+        string ErrMsg;
+
+        Clear_Fields();
+        if (Q_Forename.Text.Trim() == "" && Q_Surname.Text.Trim() == ""
+         && Q_Email.Text.Trim() == "")
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "print", "<script>alert('Must input at least one query condition!')</script>");
+            return;
+        }
+
+
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ToString();
+        SqlConnection conn = new SqlConnection(connectionString);
+
+       
+
+        SqlCommand myCommand = new SqlCommand("sp_queryCompetency", conn);
+        myCommand.CommandType = CommandType.StoredProcedure;
+
+        SqlParameter cmdParameter = new SqlParameter("@ForeName", SqlDbType.VarChar, 50);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = Q_Forename.Text;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@Surname", SqlDbType.VarChar, 50);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = Q_Surname.Text;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@Email", SqlDbType.VarChar, 100);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = Q_Email.Text;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@EmployeeID", SqlDbType.Int);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@Position", SqlDbType.VarChar,10);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@HireDate", SqlDbType.Date);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@YearsInIndustry", SqlDbType.Int);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@DriverLicenseClass", SqlDbType.VarChar,10);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@SiteSafePhoto", SqlDbType.VarChar, 255);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@Qualifications", SqlDbType.VarChar, 1024);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@GeneralCompetency", SqlDbType.VarChar, 7);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@ToolCompetency", SqlDbType.VarChar, 7);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@ErrCode", SqlDbType.Int);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@ErrMsg", SqlDbType.VarChar, 60);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        conn.Open();
+
+        myCommand.ExecuteNonQuery();
+
+
+        ErrCode = (int)myCommand.Parameters["@ErrCode"].Value;
+        ErrMsg = (string)myCommand.Parameters["@ErrMsg"].Value;
+
+        if (ErrCode != 0)
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('Add Error!')</script>");
+            conn.Close();
+            return;
+        }
+        else
+        {
+            EmployeeID.Value = myCommand.Parameters["@EmployeeID"].Value.ToString();
+            Position.Text = (string)myCommand.Parameters["@Position"].Value;
+            HireDate.Text = ((DateTime)(myCommand.Parameters["@HireDate"].Value)).ToString("yyyymmdd");
+            YearsInIndustry.Text = myCommand.Parameters["@YearsInIndustry"].Value.ToString();
+            SiteSafe.ImageUrl = myCommand.Parameters["@SiteSafePhoto"].Value.ToString();
+            Qualifications.Text = (string)myCommand.Parameters["@Qualifications"].Value;
+            GeneralCompetency.Text = (string)myCommand.Parameters["@GeneralCompetency"].Value;
+            ToolCompetency.Text = (string)myCommand.Parameters["@ToolCompetency"].Value;
+        }
+        
+    }
+
+    protected void Add_Click(object sender, EventArgs e)
+    {
+        int ErrCode;
+        string ErrMsg;
+
+
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ToString();
+        SqlConnection conn = new SqlConnection(connectionString);
+
+        SqlCommand myCommand = new SqlCommand("sp_addCompetency", conn);
+        myCommand.CommandType = CommandType.StoredProcedure;
+
+        SqlParameter cmdParameter = new SqlParameter("@EmployeeID", SqlDbType.VarChar, 50);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = EmployeeID.Value;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@Qualifications", SqlDbType.VarChar, 50);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = Qualifications.Text.Trim();
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@GeneralCompetency", SqlDbType.VarChar, 100);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = GeneralCompetency.SelectedValue;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@ToolCompetency", SqlDbType.VarChar, 8);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = ToolCompetency.SelectedValue;
+        myCommand.Parameters.Add(cmdParameter);
+
+        //////////////////////////////////////////////////////////
+        cmdParameter = new SqlParameter("@ErrCode", SqlDbType.Int);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@ErrMsg", SqlDbType.VarChar, 60);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        conn.Open();
+
+        myCommand.ExecuteNonQuery();
+
+
+        ErrCode = (int)myCommand.Parameters["@ErrCode"].Value;
+        ErrMsg = (string)myCommand.Parameters["@ErrMsg"].Value;
+
+        conn.Close();
+
+        if (ErrCode != 0)
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('Add Error!')</script>");
+
+        }
+        else
+        {
+            Clear_Fields();
+            ClientScript.RegisterStartupScript(typeof(string), "success", "<script>alert('Add successfully!')</script>");         
+        }
+    }
+
+    protected void Delete_Click(object sender, EventArgs e)
+    {
+        int ErrCode;
+        string ErrMsg;
+
+
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ToString();
+        SqlConnection conn = new SqlConnection(connectionString);
+
+        SqlCommand myCommand = new SqlCommand("sp_deleteCompetency", conn);
+        myCommand.CommandType = CommandType.StoredProcedure;
+
+        SqlParameter cmdParameter = new SqlParameter("@EmployeeID", SqlDbType.VarChar, 50);
+        cmdParameter.Direction = ParameterDirection.Input;
+        cmdParameter.Value = EmployeeID.Value;
+        myCommand.Parameters.Add(cmdParameter);
+   
+        //////////////////////////////////////////////////////////
+        cmdParameter = new SqlParameter("@ErrCode", SqlDbType.Int);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        cmdParameter = new SqlParameter("@ErrMsg", SqlDbType.VarChar, 60);
+        cmdParameter.Direction = ParameterDirection.Output;
+        myCommand.Parameters.Add(cmdParameter);
+
+        conn.Open();
+
+        myCommand.ExecuteNonQuery();
+
+
+        ErrCode = (int)myCommand.Parameters["@ErrCode"].Value;
+        ErrMsg = (string)myCommand.Parameters["@ErrMsg"].Value;
+
+        conn.Close();
+
+        if (ErrCode != 0)
+        {
+            ClientScript.RegisterStartupScript(typeof(string), "error", "<script>alert('Delete Error!')</script>");
+
+        }
+        else
+        {
+            Clear_Fields();
+            ClientScript.RegisterStartupScript(typeof(string), "success", "<script>alert('Delete successfully!')</script>");
+        }
+
+    }
+
+    protected void Export_Click(object sender, EventArgs e)
+    {
+
+    }
+}
