dc54caa65923358e0f7d78c3d39ac95c51750cc8
TaoSun2016 <kevinsun9516@gmail.com>
2017-05-12 15:21:29 +1200

    add new stored procedure to update employee infor

---
+++ 02_DBDesign/DBScript/sp_updateEmployee.sql
@@ -0,0 +1,89 @@
+use HRMSDB
+go
+drop procedure sp_updateEmployee
+go
+create procedure sp_updateEmployee(
+	@EmployeeID	int,
+	@Forename			varchar(50),
+	@Surname			varchar(50),
+	@DOB				date,
+	@Position			varchar(10),
+	@OtherPositions		varchar(128),
+	@HireDate			date,
+	@ResignDate			date,
+	@YearsInBCD			int,
+	@YearsInIndustry	int,
+	@DriverLicenseNumber		varchar(30),
+	@DriverLicenseClass			varchar(10),
+	@DriverLicenseExpiryDate	date,
+	@DriverLicensePhoto			varchar(255),
+	@SiteSafeNumber				varchar(50),
+	@SiteSafeExpiryDate			date,
+	@SiteSafePhoto		varchar(255),
+	@MobileNumber		varchar(20),
+	@HomeNumber			varchar(20),
+	@Email				varchar(100),
+	@KinName			varchar(100),
+	@KinNumber			varchar(20),
+	@BankName			varchar(30),
+	@AccountNumber		varchar(30),
+	@PayRate			int,
+	@TaxRate			int,
+	@IRDNumber			varchar(20),
+	@Country			varchar(30),
+	@City				varchar(30),
+	@Suburb				varchar(30),
+	@Street				varchar(100),
+	@PostCode			varchar(10),
+	@Note				varchar(255),
+
+	@ErrCode int output,
+	@ErrMsg varchar(60)output)
+as
+begin
+	begin try
+		update EMPLOYEE set Forename = @Forename
+						, Surname = @Surname
+						, DOB = @DOB
+						, Position = @Position
+						, OtherPositions = @OtherPositions
+						, HireDate = @HireDate
+						, ResignDate = @ResignDate
+						, YearsInBCD = @YearsInBCD
+						, YearsInIndustry = @YearsInIndustry
+						, DriverLicenseNumber = @DriverLicenseNumber
+						, SiteSafeNumber = @SiteSafeNumber
+						, DriverLicenseClass = @DriverLicenseClass
+						, DriverLicenseExpiryDate = @DriverLicenseExpiryDate
+						, SiteSafeExpiryDate = @SiteSafeExpiryDate
+						, DriverLicensePhoto = @DriverLicensePhoto
+						, SiteSafePhoto = @SiteSafePhoto
+						, MobileNumber = @MobileNumber
+						, HomeNumber = @HomeNumber
+						, Email = @Email
+						, KinName = @KinName
+						, KinNumber = @KinNumber
+						, BankName = @BankName
+						, AccountNumber = @AccountNumber
+						, PayRate = @PayRate
+						, TaxRate = @TaxRate
+						, IRDNumber = @IRDNumber
+						, Country = @Country
+						, City = @City
+						, Suburb = @Suburb
+						, Street = @Street
+						, PostCode = @PostCode
+						, Note = @Note
+						where EmployeeID = @EmployeeID
+	end try
+	begin catch
+		insert into LOGTBL values (GETDATE(),ERROR_PROCEDURE(),ERROR_LINE(),ERROR_MESSAGE());
+		set @ErrCode = -1;
+		set @ErrMsg = 'Update employee error!';
+		return;
+	end catch
+	set @ErrCode = 0;
+	set @ErrMsg = 'Update employee successfully!';
+end
+
+go
